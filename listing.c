114e6bdb509b74fd0e15250f4912eb35dc2ab814
Michael Pangburn <michaelpangburn@comcast.net>
2018-02-28 20:24:20 -0800

    listing in baby steps part 2

--- listing.c
+++ listing.c
@@ -31,33 +31,35 @@ void get_permissions(char permissions[], mode_t mode, FILE *tarfile){
     permissions[index] = '\0';
 }
 
-/* figure out how to handled name lengths */
-void get_owner(uid_t uid, char uname[], gid_t gid, char gname[], char owner[]){
+/* get owner string from uid and gid */
+void get_owner(uid_t uid, char uname[], gid_t gid, char gname[], char owner[]) {
     #define OWNER_WIDTH 17
     #define MAX_NAME_LENGTH 8
     #define USER_NAME_LENGTH 32
+
     char buffer[USER_NAME_LENGTH];
+
     owner[0] = '\0';
-    if(uname[0] == '\0'){
+    if (uname[0] == '\0') {
         sprintf(buffer, "%d", uid);
         strcat(owner, buffer);
-    }
-    else{
+    } else {
         snprintf(buffer, MAX_NAME_LENGTH, "%s", uname);
         strcat(owner, buffer);
     }
+
     strcat(owner, "/");
-    if(gname[0] == '\0'){
+
+    if (gname[0] == '\0') {
         sprintf(buffer, "%d", gid);
         strcat(owner, buffer);
-    }
-    else{
+    } else {
         snprintf(buffer, MAX_NAME_LENGTH, "%s", gname);
         strcat(owner, buffer);
     }
 }
 
-
+/* get time string in yyyy:mm:dd hh:mm format */
 void get_time(time_t time, char timestr[]){
     struct tm *tm = localtime(&time);
     int year;
@@ -75,78 +77,78 @@ void get_time(time_t time, char timestr[]){
 }
 
 
-/* mode = 0 for standard, verbose otherwise */
+/* list the contents of a file */
 void list_contents(FILE* tarfile, char path[], int isverbose, int isstrict){
     #define EXTRA_SPACE (BLOCK_LENGTH - HEADER_LENGTH)
     #define PERMISSION_WIDTH 11 
     #define OWNER_WIDTH 17
     #define TIME_WIDTH 17
-    int size;
+    mode_t mode;
+    off_t size;
+    time_t time;
     uid_t uid;
     gid_t gid;
-    int blocks;
-    time_t time;
     char gname[GNAME_LENGTH];
     char uname[UNAME_LENGTH];
     char timestr[TIME_WIDTH];
     char owner[OWNER_WIDTH];
-    char buffer[PATH_MAX];
     char permissions[PERMISSION_WIDTH];
-    mode_t mode;
+    char buffer[PATH_MAX];
+    int blocks;
+
     buffer[0] = '\0';
     /* get permissions */
     if(validate_header(tarfile, isstrict) != 0){
-	fprintf(stderr, "Invalid Header\n");
-	exit(EXIT_FAILURE);
+        fprintf(stderr, "Invalid Header\n");
+        exit(EXIT_FAILURE);
     }
     
     mode = get_mode(tarfile);
     /* get size */
     size = get_size(tarfile);
     if(isverbose){
-	/* verbose print */
-	get_permissions(permissions, mode, tarfile);
-	
-	fseek(tarfile, UID_OFFSET, SEEK_CUR);
-	fread(owner, 1, UID_LENGTH, tarfile);
-	uid = strtol(owner, NULL, 8);
-	fread(owner, 1, GID_LENGTH, tarfile);
-	gid = strtol(owner, NULL, 8);
-	fseek(tarfile, -GID_OFFSET - GID_LENGTH, SEEK_CUR);
-	fseek(tarfile, UNAME_OFFSET, SEEK_CUR);
-	fread(uname, 1, UNAME_LENGTH, tarfile);
-	fread(gname, 1, GNAME_LENGTH, tarfile);
-	fseek(tarfile, -GNAME_LENGTH - GNAME_OFFSET, SEEK_CUR);
-	get_owner(uid, uname, gid, gname, owner);
+        /* verbose print */
+        get_permissions(permissions, mode, tarfile);
 
-	fseek(tarfile, MTIME_OFFSET, SEEK_CUR);
-	fread(buffer, 1, MTIME_LENGTH, tarfile);
-	time = strtol(buffer, NULL, 8);
-	get_time(time, timestr);
+        fseek(tarfile, UID_OFFSET, SEEK_CUR);
+        fread(owner, 1, UID_LENGTH, tarfile);
+        uid = (uid_t)strtol(owner, NULL, 8);
+        fread(owner, 1, GID_LENGTH, tarfile);
+        gid = (gid_t)strtol(owner, NULL, 8);
+        fseek(tarfile, -GID_OFFSET - GID_LENGTH, SEEK_CUR);
+        fseek(tarfile, UNAME_OFFSET, SEEK_CUR);
+        fread(uname, 1, UNAME_LENGTH, tarfile);
+        fread(gname, 1, GNAME_LENGTH, tarfile);
+        fseek(tarfile, -GNAME_LENGTH - GNAME_OFFSET, SEEK_CUR);
+        get_owner(uid, uname, gid, gname, owner);
 
-	printf("%10s %-17s %8d %16s %s\n", permissions, owner,
-	       size, timestr, path);
+        fseek(tarfile, MTIME_OFFSET, SEEK_CUR);
+        fread(buffer, 1, MTIME_LENGTH, tarfile);
+        time = strtol(buffer, NULL, 8);
+        get_time(time, timestr);
 
-	fseek(tarfile, -MTIME_OFFSET - MTIME_LENGTH, SEEK_CUR); 
-    }else{
-	/* standard print */
-	puts(path);
+        printf("%10s %-17s %8d %16s %s\n", permissions, owner,
+               (int)size, timestr, path);
+
+        fseek(tarfile, -MTIME_OFFSET - MTIME_LENGTH, SEEK_CUR);
+    } else {
+        /* standard print */
+        puts(path);
     }
+
     /* go to next header */
     if(!is_dir(tarfile)){
-	blocks = size_to_blocks(size);
+        blocks = size_to_blocks(size);
         fseek(tarfile, blocks * BLOCK_LENGTH + BLOCK_LENGTH, SEEK_CUR);
-    }
-   
-    /* next entry */
-    else{
-	fseek(tarfile, BLOCK_LENGTH, SEEK_CUR);
+    } else {
+        /* check next entry */
+        fseek(tarfile, BLOCK_LENGTH, SEEK_CUR);
         get_path(buffer, tarfile);
         while (strncmp(buffer, path, strlen(path)) == 0) {
             list_contents(tarfile, buffer, isverbose, isstrict);
             get_path(buffer, tarfile);
-	    if(buffer[0] == '\0')
-		return;
+            if(buffer[0] == '\0')
+                return;
         }
     }
 }
